16090d644611682fdee3ba15706282fd3c38d48a
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-03-26 08:28:29 +0530

    02Installation and Folder Structure Demystified code added

---
+++ 02Installation-FolderStructure-in-NextJS/README.md
@@ -0,0 +1,3 @@
+# Installation and Folder Structure Demystified in NextJS
+
+`npx create-next-app@latest`
---
+++ 02Installation-FolderStructure-in-NextJS/my-app/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-white text-4xl text-center mt-4">Homepage Component</h1>
+      <p className="w-[800px] mx-auto text-xl p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa repudiandae laudantium libero voluptate totam, consequuntur officia asperiores odit aliquid tempore, beatae porro doloribus quos provident unde voluptas commodi sit. Rem vero minima ex aperiam. Eos tempora, aliquam, vitae amet animi quibusdam, placeat inventore maiores odit blanditiis similique. Tempora facere beatae dicta maxime libero omnis?</p>
+    </div>
+  );
+}
